fea8013c781dd699cf7151abdd9e460f0e16bf26
xuqg <xuqg2008@163.com>
2021-09-03 18:28:15 +0800

    第一个gitdemo

---
+++ testGit/gitDemo1/src/com/xuqg/pojo/Person.java
@@ -0,0 +1,15 @@
+package com.xuqg.pojo;
+
+/**
+ * ClassName: Person
+ * Description:
+ * date: 2021/9/3 18:26
+ *
+ * @author Xu Qian gang
+ * @version 1.0
+ * @since JDK 1.8
+ */
+public class Person {
+    private String name;
+    private Integer age;
+}
